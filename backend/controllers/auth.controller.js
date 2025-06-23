9f6397c44e57d29272e8e13c95714f66cd3a72f0
nethal17 <it23259102@my.sliit.lk>
2025-06-23 18:53:46 +0530

    Initial Commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,26 @@
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+    const {email, name, password} = req.body;
+
+    try {
+        const userExists = await User.findOne({email});
+
+        if (userExists) {
+            return res.status(400).json({message: "User already exists"});
+        }
+        const user = await User.create({email, name, password});
+
+        res.status(201).json({user, message: "User created successfully"});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+};
+
+export const login = async (req, res) => {
+    res.send("Login route called");
+}
+
+export const logout = async (req, res) => {
+    res.send("Logout route called");
+}      
