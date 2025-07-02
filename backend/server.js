15acc228f8b9e1ed1fadb001d453619d99dd6984
nethal17 <it23259102@my.sliit.lk>
2025-07-02 22:55:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,13 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
+app.get('/', (req,res) => {
+	res.send({
+		activeStatus: true,
+		error: false
+	});
+});
+
 app.listen(PORT, () => {
 	console.log("Server is running on http://localhost:" + PORT);
 	connectDB();
