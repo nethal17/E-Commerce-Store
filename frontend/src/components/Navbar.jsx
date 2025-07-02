412c9a30cf2a4997a5eef34ca689569aa053dab3
nethal17 <it23259102@my.sliit.lk>
2025-07-02 13:18:26 +0530

    Fix navbar icons

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,29 +16,25 @@ const Navbar = () => {
 
 	return (
 		<header className='fixed top-0 left-0 z-40 w-full transition-all duration-300 bg-gray-900 border-b shadow-lg bg-opacity-90 backdrop-blur-md border-sky-800'>
-			<div className='container px-4 py-3 mx-auto'>
+			<div className='container px-2 py-3 mx-4'>
 				<div className='flex flex-wrap items-center justify-between'>
 					<Link to='/' className='flex items-center space-x-2 text-2xl font-bold text-sky-400'>
 						E-Commerce
 					</Link>
 
-					<nav className='flex flex-wrap items-center gap-6'>
-						<Link
-							to={"/"}
-							className='text-gray-300 transition duration-300 ease-in-out hover:text-sky-400'
-						>
-							Home
-						</Link>
+					<nav className='flex flex-wrap items-center gap-6 mx-[-140px]'>
+						
+						
 						{user && (
 							<Link
 								to={"/cart"}
 								className='relative text-gray-300 transition duration-300 ease-in-out group hover:text-sky-400'
 							>
-								<ShoppingCart className='inline-block mr-1 group-hover:text-sky-400' size={20} />
+								<ShoppingCart className='inline-block mr-1 group-hover:text-sky-400' size={26} />
 								<span className='hidden sm:inline'>Cart</span>
 								{cart.length > 0 && (
 									<span
-										className='absolute -top-2 -left-2 bg-sky-500 text-white rounded-full px-2 py-0.5 
+										className='absolute -top-3 -left-3 bg-sky-500 text-white rounded-full px-2 py-0.5 
 									text-xs group-hover:bg-sky-400 transition duration-300 ease-in-out'
 									>
 										{cart.length}
@@ -48,10 +44,10 @@ const Navbar = () => {
 						)}
 						{isAdmin && (
 							<Link
-								className='flex items-center px-3 py-1 font-medium text-white transition duration-300 ease-in-out rounded-md bg-sky-700 hover:bg-sky-600'
+								className='flex items-center px-2 py-2 font-medium text-white transition duration-300 ease-in-out rounded-md bg-sky-700 hover:bg-sky-600'
 								to={"/secret-dashboard"}
 							>
-								<Lock className='inline-block mr-1' size={18} />
+								<Lock className='inline-block mr-2' size={20} />
 								<span className='hidden sm:inline'>Dashboard</span>
 							</Link>
 						)}
@@ -75,7 +71,7 @@ const Navbar = () => {
 								</Link>
 								<Link
 									to={"/login"}
-									className='flex items-center px-4 py-2 text-white transition duration-300 ease-in-out bg-gray-700 rounded-md hover:bg-gray-500'
+									className='flex items-center px-6 py-2 text-white transition duration-300 ease-in-out bg-gray-700 rounded-md hover:bg-gray-500'
 								>
 									<LogIn className='mr-2' size={22} />
 									Login
